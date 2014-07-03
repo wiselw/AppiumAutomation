38479f8ef3fffcea559b6f9039e821c6386f3065
wiselw <wiselw@gmail.com>
2014-07-03 16:17:25 +0800

    调整格式

--- testNGAppium/src/main/java/com/merlini/app/common/AppiumDriverWait.java
+++ testNGAppium/src/main/java/com/merlini/app/common/AppiumDriverWait.java
@@ -16,51 +16,51 @@ import com.merlini.common.Config;
 
 public class AppiumDriverWait extends FluentWait<AppiumDriver>{
 	//默认轮询时间(毫秒)
-		public final static long DEFAULT_POLLINGEVERY_TIMEMILLS = Integer.parseInt(Config.getConfBykey("wait.sleepInMillis"));
-		public final static long DEFAULT_TIMEOUT_INSECONDS = Integer.parseInt(Config.getConfBykey("wait.TimeOutInSeconds"));
-		/**
-		   * Wait will ignore instances of NotFoundException that are encountered (thrown) by default in
-		   * the 'until' condition, and immediately propagate all others.  You can add more to the ignore
-		   * list by calling ignoring(exceptions to add).
-		   *
-		   * @param driver The AppiumDriver instance to pass to the expected conditions
-		   * @see WebDriverWait#ignoring(java.lang.Class)
-		   */
-		public AppiumDriverWait(AppiumDriver driver) {
-		    this(driver, new SystemClock(), Sleeper.SYSTEM_SLEEPER, DEFAULT_TIMEOUT_INSECONDS, DEFAULT_POLLINGEVERY_TIMEMILLS);
-		}
-		/**
-		   * Wait will ignore instances of NotFoundException that are encountered (thrown) by default in
-		   * the 'until' condition, and immediately propagate all others.  You can add more to the ignore
-		   * list by calling ignoring(exceptions to add).
-		   *
-		   * @param driver The AppiumDriver instance to pass to the expected conditions
-		   * @param timeOutInSeconds The timeout in seconds when an expectation is called
-		   * @see WebDriverWait#ignoring(java.lang.Class)
-		   */
-		public AppiumDriverWait(AppiumDriver driver, long timeOutInSeconds) {
-		    this(driver, new SystemClock(), Sleeper.SYSTEM_SLEEPER, timeOutInSeconds, DEFAULT_POLLINGEVERY_TIMEMILLS);
-		}
-		/**
-		   * Wait will ignore instances of NotFoundException that are encountered (thrown) by default in
-		   * the 'until' condition, and immediately propagate all others.  You can add more to the ignore
-		   * list by calling ignoring(exceptions to add).
-		   *
-		   * @param driver The WebDriver instance to pass to the expected conditions
-		   * @param timeOutInSeconds The timeout in seconds when an expectation is called
-		   * @param sleepInMillis The duration in milliseconds to sleep between polls.
-		   * @see WebDriverWait#ignoring(java.lang.Class)
-		   */
-		public AppiumDriverWait(AppiumDriver driver, long timeOutInSeconds, long sleepInMillis) {
-		    this(driver, new SystemClock(), Sleeper.SYSTEM_SLEEPER, timeOutInSeconds, sleepInMillis);
-		  }
-		protected AppiumDriverWait(AppiumDriver driver, Clock clock, Sleeper sleeper, long timeOutInSeconds,
-			  long sleepTimeOut) {
-			super(driver, clock, sleeper);
-			withTimeout(timeOutInSeconds, TimeUnit.SECONDS);
-			pollingEvery(sleepTimeOut, TimeUnit.MILLISECONDS);
-			ignoring(NotFoundException.class);
-		}
+	public final static long DEFAULT_POLLINGEVERY_TIMEMILLS = Integer.parseInt(Config.getConfBykey("wait.sleepInMillis"));
+	public final static long DEFAULT_TIMEOUT_INSECONDS = Integer.parseInt(Config.getConfBykey("wait.TimeOutInSeconds"));
+	/**
+	   * Wait will ignore instances of NotFoundException that are encountered (thrown) by default in
+	   * the 'until' condition, and immediately propagate all others.  You can add more to the ignore
+	   * list by calling ignoring(exceptions to add).
+	   *
+	   * @param driver The AppiumDriver instance to pass to the expected conditions
+	   * @see WebDriverWait#ignoring(java.lang.Class)
+	   */
+	public AppiumDriverWait(AppiumDriver driver) {
+	    this(driver, new SystemClock(), Sleeper.SYSTEM_SLEEPER, DEFAULT_TIMEOUT_INSECONDS, DEFAULT_POLLINGEVERY_TIMEMILLS);
+	}
+	/**
+	   * Wait will ignore instances of NotFoundException that are encountered (thrown) by default in
+	   * the 'until' condition, and immediately propagate all others.  You can add more to the ignore
+	   * list by calling ignoring(exceptions to add).
+	   *
+	   * @param driver The AppiumDriver instance to pass to the expected conditions
+	   * @param timeOutInSeconds The timeout in seconds when an expectation is called
+	   * @see WebDriverWait#ignoring(java.lang.Class)
+	   */
+	public AppiumDriverWait(AppiumDriver driver, long timeOutInSeconds) {
+	    this(driver, new SystemClock(), Sleeper.SYSTEM_SLEEPER, timeOutInSeconds, DEFAULT_POLLINGEVERY_TIMEMILLS);
+	}
+	/**
+	   * Wait will ignore instances of NotFoundException that are encountered (thrown) by default in
+	   * the 'until' condition, and immediately propagate all others.  You can add more to the ignore
+	   * list by calling ignoring(exceptions to add).
+	   *
+	   * @param driver The WebDriver instance to pass to the expected conditions
+	   * @param timeOutInSeconds The timeout in seconds when an expectation is called
+	   * @param sleepInMillis The duration in milliseconds to sleep between polls.
+	   * @see WebDriverWait#ignoring(java.lang.Class)
+	   */
+	public AppiumDriverWait(AppiumDriver driver, long timeOutInSeconds, long sleepInMillis) {
+	    this(driver, new SystemClock(), Sleeper.SYSTEM_SLEEPER, timeOutInSeconds, sleepInMillis);
+	  }
+	protected AppiumDriverWait(AppiumDriver driver, Clock clock, Sleeper sleeper, long timeOutInSeconds,
+		  long sleepTimeOut) {
+		super(driver, clock, sleeper);
+		withTimeout(timeOutInSeconds, TimeUnit.SECONDS);
+		pollingEvery(sleepTimeOut, TimeUnit.MILLISECONDS);
+		ignoring(NotFoundException.class);
+	}
 
 
 }
